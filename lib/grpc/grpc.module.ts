bd0246428d6cb4dee92b8cc85a0fccef68db02d4
Lev <lev.kovryzhko@bk.ru>
2026-03-10 13:42:12 +0300

    fix: di

--- lib/grpc/grpc.module.ts
+++ lib/grpc/grpc.module.ts
@@ -2,17 +2,18 @@ import { DynamicModule, Inject, Module } from "@nestjs/common";
 import { GRPC_CLIENTS } from "./grpc.registry";
 import { GrpcClientFactory } from "./grpc-client.factory";
 import { GRPC_CLIENT_PREFIX } from "./grpc.const";
-import { ConfigService } from "@nestjs/config";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
 @Module({})
 export class GrpcModule {
-    public static register(clients: Array<keyof typeof GRPC_CLIENTS>): DynamicModule {
+    static register(clients: Array<keyof typeof GRPC_CLIENTS>): DynamicModule {
         return {
             module: GrpcModule,
+            imports: [ConfigModule],
             providers: [
                 GrpcClientFactory,
                 ...clients.map(token => {
-                    const cfg = GRPC_CLIENTS[token]
+                    const cfg = GRPC_CLIENTS[token];
 
                     return {
                         provide: `${GRPC_CLIENT_PREFIX}_${token}`,
@@ -20,26 +21,26 @@ export class GrpcModule {
                             factory: GrpcClientFactory,
                             config: ConfigService,
                         ) => {
-                            const url = config.getOrThrow(cfg.env)
+                            const url = config.getOrThrow(cfg.env);
 
                             const client = factory.createClient({
                                 package: cfg.package,
                                 protoPath: cfg.protoPath,
                                 url,
-                            })
+                            });
 
-                            factory.register(token, client)
+                            factory.register(token, client);
 
-                            return client
+                            return client;
                         },
-                        inject: [GrpcClientFactory, ConfigService]
-                    }
-                })
+                        inject: [GrpcClientFactory, ConfigService],
+                    };
+                }),
             ],
             exports: [
                 GrpcClientFactory,
-                ...clients.map(token => `${GRPC_CLIENT_PREFIX}_${token}`)
-            ]
-        }
+                ...clients.map(token => `${GRPC_CLIENT_PREFIX}_${token}`),
+            ],
+        };
     }
 }
