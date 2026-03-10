ac0d922e8e8581f1e5cc086d80119391769fd579
Lev <lev.kovryzhko@bk.ru>
2026-03-10 13:47:24 +0300

    fix: registry name

--- lib/grpc/grpc.registry.ts
+++ lib/grpc/grpc.registry.ts
@@ -4,16 +4,16 @@ export const GRPC_CLIENTS = {
     AUTH_PACKAGE: {
         package: 'auth.v1',
         protoPath: PROTO_PATHS.AUTH,
-        env: 'AUTH_GPRC_URL',
+        env: 'AUTH_GRPC_URL',
     },
     ACCOUNT_PACKAGE: {
         package: 'account.v1',
         protoPath: PROTO_PATHS.ACCOUNT,
-        env: 'AUTH_GPRC_URL',
+        env: 'AUTH_GRPC_URL',
     },
     USER_PACKAGE: {
         package: 'user.v1',
         protoPath: PROTO_PATHS.USER,
-        env: 'USER_GPRC_URL',
+        env: 'USER_GRPC_URL',
     },
 } as const
